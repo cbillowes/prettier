335e1e3f13cdfffbd73c8b1fcb961493543765e7
Clarice Bouwer <clarice@simply.co.za>
2022-04-16 05:17:27 +0400

    add location switch -L in curl command

--- install.sh
+++ install.sh
@@ -16,13 +16,11 @@ echo "     |  _ \ _ __ ___| |_| |_(_) ___ _ __ "
 echo "     | |_) | '__/ _ \ __| __| |/ _ \ '__|"
 echo "    _|  __/| | |  __/ |_| |_| |  __/ |   "
 echo "   (_)_|   |_|  \___|\__|\__|_|\___|_|   "
-echo "   OUTPUT_DIR: $OUTPUT_DIR"
+echo "   DROPPING IN TO $OUTPUT_DIR"
 echo
 
-echo "Dropping in..."
-
-curl $EDITOR_FILE >$OUTPUT_DIR/.editorconfig
-curl $PRETTIER_IGNORE_FILE >$OUTPUT_DIR/.prettierignore
-curl $PRETTIERRC_FILE >$OUTPUT_DIR/.prettierrc
+curl -L $EDITOR_FILE >$OUTPUT_DIR/.editorconfig
+curl -L $PRETTIER_IGNORE_FILE >$OUTPUT_DIR/.prettierignore
+curl -L $PRETTIERRC_FILE >$OUTPUT_DIR/.prettierrc
 
 echo "Done! 🎉"
