aeea5e723cb2f72ad800754fe526e2dc2db2cf85
Clarice Bouwer <cbillowes@gmail.com>
2022-04-16 05:02:40 +0400

    verbosify install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-REPOSITORY="https://github.com/cbillowes/prettier/blob/main"
+REPOSITORY="https://raw.githubusercontent.com/cbillowes/prettier/main/"
 EDITOR_FILE="$REPOSITORY/.editorconfig"
 PRETTIER_IGNORE_FILE="$REPOSITORY/.prettierignore"
 PRETTIERRC_FILE="$REPOSITORY/.prettierrc"
 
 echo "Drop-in prettier config from $REPOSITORY."
-echo "Usage: $(basename $0) <path> - path to copy to relative to present working directory"
+echo "Usage: ./install.sh <path> - path to copy to relative to present working directory"
 
 OUTPUT_DIR=$(cd $(dirname $1) && pwd)/$(basename $1)
 
+echo
+echo "      ____           _   _   _           "
+echo "     |  _ \ _ __ ___| |_| |_(_) ___ _ __ "
+echo "     | |_) | '__/ _ \ __| __| |/ _ \ '__|"
+echo "    _|  __/| | |  __/ |_| |_| |  __/ |   "
+echo "   (_)_|   |_|  \___|\__|\__|_|\___|_|   "
+echo "   OUTPUT_DIR: $OUTPUT_DIR"
+echo
+
 echo "Dropping in..."
-curl -s $EDITOR_FILE >$OUTPUT_DIR/.editorconfig
-curl -s $PRETTIER_IGNORE_FILE >$OUTPUT_DIR/.prettierignore
-curl -s $PRETTIERRC_FILE >$OUTPUT_DIR/.prettierrc
+
+curl $EDITOR_FILE >$OUTPUT_DIR/.editorconfig
+curl $PRETTIER_IGNORE_FILE >$OUTPUT_DIR/.prettierignore
+curl $PRETTIERRC_FILE >$OUTPUT_DIR/.prettierrc
 
 echo "Done! 🎉"
